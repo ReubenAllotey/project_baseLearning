ca55b8e23d9a06c35c4b2649a1240224284df539
Reuben Allotey <reubenallotey434@gmail.com>
2026-03-09 10:40:42 +0000

    this is a to do list , with a database been stored as well

---
+++ Permalist/queries.sql
@@ -0,0 +1,10 @@
+
+create database permalist
+
+
+CREATE TABLE items (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(100) NOT NULL
+);
+
+INSERT INTO items (title) VALUES ('Buy milk'), ('Finish homework')

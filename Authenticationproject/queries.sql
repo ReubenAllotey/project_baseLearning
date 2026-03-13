2211647b81d7df6d6636b5d1057c750590a4d0e6
Reuben Allotey <reubenallotey434@gmail.com>
2026-03-13 00:34:22 +0000

    secret project where user can register and login locally and with  google

---
+++ Authenticationproject/queries.sql
@@ -0,0 +1,12 @@
+create database secrets
+
+
+
+CREATE TABLE users(
+id SERIAL PRIMARY KEY,
+email VARCHAR(100) NOT NULL UNIQUE,
+password VARCHAR(100)
+);
+
+ALTER TABLE users
+ADD  secrets VARCHAR(300);
